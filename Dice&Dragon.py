2db4939b7d000b788da65a991385736dd0d74cf5
blopablo <olbap0991@gmail.com>
2025-06-26 19:46:17 -0300

    Ahora con sonido de inicio y en los clics

--- Dice&Dragon.py
+++ Dice&Dragon.py
@@ -1,8 +1,21 @@
 import tkinter as tk
 from random import randint
+import pygame
 
 class Dado:
     def __init__(self):
+
+        pygame.mixer.init()
+        pygame.mixer.music.load("sound/dragon_breath.mp3")
+        pygame.mixer.music.play(1)  # - - - - Elementos para agregar musica o sonidos mp3 o wav
+
+        self.sonido_click = pygame.mixer.Sound("sound/clic/dragon_roar.mp3",)
+        self.sonido_click1 = pygame.mixer.Sound("sound/clic/D6.mp3",)
+        self.sonido_click2 = pygame.mixer.Sound("sound/clic/D10.mp3",)
+        self.sonido_click3 = pygame.mixer.Sound("sound/clic/D20.mp3",)
+        self.sonido_click4 = pygame.mixer.Sound("sound/clic/D100.mp3",)
+        
+
         self.ventana = tk.Tk()
         self.ventana.title("Dice & Dragon")
         self.ventana.geometry("400x500")
@@ -20,7 +33,7 @@ class Dado:
 
         # Crear los demás widgets
         self.etiqueta = tk.Label(self.ventana, text="ReSuLtAdO", font=("Arial", 30), bg="black", fg="darkgoldenrod1")
-        self.etiqueta.place(x=165, y=430)
+        self.etiqueta.place(x=175, y=430)
 
         botones = [
             ("Lanzar Ð4", self.lanzar_d4),
@@ -35,24 +48,29 @@ class Dado:
             b.place(x=10, y=100 + i * 60)
 
     def lanzar_d4(self):
+        self.sonido_click.play()
         resultado = randint(1, 4)
-        self.etiqueta["text"] = f"Ð4=> {resultado}  "
+        self.etiqueta["text"] = f"Ð 4 = {resultado}"
 
     def lanzar_d6(self):
+        self.sonido_click1.play()
         resultado = randint(1, 6)
-        self.etiqueta["text"] = f"Ð6=> {resultado}  "
+        self.etiqueta["text"] = f"Ð 6 = {resultado}"
 
     def lanzar_d10(self):
+        self.sonido_click2.play()
         resultado = randint(1, 10)
-        self.etiqueta["text"] = f"Ð10=> {resultado} "
+        self.etiqueta["text"] = f"Ð10 = {resultado}"
 
     def lanzar_d20(self):
+        self.sonido_click3.play()
         resultado = randint(1, 20)
-        self.etiqueta["text"] = f"Ð20=> {resultado} "
+        self.etiqueta["text"] = f"Ð20 = {resultado}"
 
     def lanzar_d100(self):
+        self.sonido_click4.play()
         resultado = randint(1, 100)
-        self.etiqueta["text"] = f"Ð100=> {resultado}"
+        self.etiqueta["text"] = f"Ð100 = {resultado}"
 
     def run(self):
         self.ventana.mainloop()
